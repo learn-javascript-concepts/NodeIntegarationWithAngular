bce1b33c6d7a9d3181c91a7273fce70e09a834f6
unknown <learn-javascript-concepts@gmail.com>
2018-02-11 22:48:31 +0530

    Login Page Integration

--- public/appScripts/appModule.js
+++ public/appScripts/appModule.js
@@ -9,7 +9,8 @@
     appModule.config(function($routeProvider, $locationProvider) {
         $locationProvider.html5Mode(true);
         $routeProvider.when("/", {
-            templateUrl : "partials/index.vash"
+            templateUrl : "partials/index.vash",
+            controller: "mainController"
         });
     });
 
